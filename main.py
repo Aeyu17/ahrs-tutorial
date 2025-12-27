d4a90c23bcd85902f747ecd4e892f36c3aae042a
Aeyu17 <jami3507@gmail.com>
2025-12-26 20:15:48 -0600

    I did it

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from import_h5 import importADPM, SensorData
-from quaternion import quatToEuler, quatNormalise
+from quaternion import quatToEuler, quatNormalise, quatToDCM, incAccel
 from matrix import skew3
 
 import numpy as np
@@ -26,18 +26,17 @@ def main():
     x = np.zeros(shape=(4, n))
     x[0, :] = np.ones(shape=(1, n))
 
+    x[:, 0] = incAccel(data.accel[0, :])
+
+    P = np.eye(4)
+    Q = np.eye(3) * gyr_noise ** 2
+    R = np.eye(3) * acc_noise ** 2
+    g = 9.81
+
     for i in range(n-1):
         # Time update
-        # x = f(x_(k-1), u_(k-1), 0) becomes...
-        # q_t = q_t-1 (*) exp_q(dt/2 * y_w,t-1) per Kok paper 4.45a
-        # exp_q = (cos |n|; n/|n| * sin|n|) ~= (1; n) where n is our gyro measurements
-        # then you do quaternion righthand multiplication, hence...
-        # def f = exp_q(dt/2 * y_w,t-1) = [1, -dt/2 * w'; dt/2 * w, I3 - skew(-dt/2 * w)];
-        #       = I4 + dt/2 [0, -w'; w, -skew(w)];
-        # then, because it's righthand mult, f * x_t-1
-
         f = np.zeros(shape=(4,4))
-        f[0, 1:] = - data.gyro[i, :] # technically transposed, but not needed heres
+        f[0, 1:] = - data.gyro[i, :] # technically transposed, but not needed here
         f[1:, 0] = data.gyro[i, :]
         f[1:, 1:] = -skew3(data.gyro[i, :])
         f *= 0.5 * dt
@@ -45,13 +44,35 @@ def main():
 
         x[:, i+1] = quatNormalise(f @ x[:, i])
 
+        # P = A*P_(k-1)*A' + W*Q_(k-1)*W'
+        A = f # why is this true? exercise left to reader, but it's in the Kok paper
+        W = np.zeros(shape=(4,3))
+        W[0, :] = - x[1:, i] # also technically transposed, but not needed
+        W[1:, :] = x[0, i] * np.eye(3) + skew3(x[1:, i])
+        P = A @ P @ A.transpose() + W @ Q @ W.transpose()
 
-        # % P = A*P_(k-1)*A' + W*Q_(k-1)*W'
+        # Measurement update
+        # K = P*H' * (H*P*H' + V*R*V')^-1
+        # H is the jacobian of the gravity vector rotated to the body frame w.r.t. the reference quaternion
 
-        # % Measurement update
-        # % K = P*H' * (H*P*H' + V*R*V')^-1
-        # % x = x - K * (z - h)
-        # % P = (I - K*H) * P
+        # H = [-2*g*xx(3,i+1),  2*g*xx(4,i+1), -2*g*xx(1,i+1), 2*g*xx(2,i+1);...
+        #       2*g*xx(2,i+1),  2*g*xx(1,i+1),  2*g*xx(4,i+1), 2*g*xx(3,i+1);...
+        #       2*g*xx(1,i+1), -2*g*xx(2,i+1), -2*g*xx(3,i+1), 2*g*xx(4,i+1)];
+
+        H = np.array([[-x[2, i+1],  x[3, i+1], -x[0, i+1],  x[1, i+1]], 
+                      [ x[1, i+1],  x[0, i+1],  x[3, i+1],  x[2, i+1]], 
+                      [ x[0, i+1], -x[1, i+1], -x[2, i+1],  x[3, i+1]]])
+        H *= 2 * g
+        K = P @ H.transpose() @ np.linalg.inv(H @ P @ H.transpose() + R)
+
+        # x = x - K * (z - h)
+        h = quatToDCM(x[:, i+1]).transpose() @ np.array([0, 0, g])
+        if i < 5:
+            print(h)
+        x[:, i+1] = x[:, i+1] + K @ (data.accel[i, :] - h)
+        x[:, i+1] = quatNormalise(x[:, i+1])
+        # P = (I - K*H) * P
+        P = (np.eye(4) - K @ H) @ P
 
     ekf_euler = quatToEuler(x.transpose())
     plt.figure()
--- quaternion.py
+++ quaternion.py
@@ -1,5 +1,5 @@
 import numpy as np
-from math import atan2, asin, sqrt
+from math import atan2, asin, sqrt, cos, sin
 
 def quatToEuler(quat: np.ndarray):
     if quat.ndim != 2 or np.shape(quat)[1] != 4:
@@ -11,6 +11,27 @@ def quatToEuler(quat: np.ndarray):
         eulerArr[i, :] = singleQuatToEuler(q)
 
     return eulerArr
+
+def incAccel(accel):
+    # %calculate pitch and roll from accel
+    # pitch=atan2(-accel(:,1),sqrt(accel(:,2).^2+accel(:,3).^2)); %pitch
+    # roll=atan2(accel(:,2),accel(:,3));  %roll
+    pitch = atan2(-accel[0], sqrt(accel[1] ** 2 + accel[2] ** 2))
+    roll = atan2(accel[1], accel[2])
+
+    # %Convert to quaternion    
+    # q(:,1)=cos(pitch./2).*cos(roll./2);
+    # q(:,2)=cos(pitch./2).*sin(roll./2);
+    # q(:,3)=sin(pitch./2).*cos(roll./2);
+    # q(:,4)=-sin(pitch./2).*sin(roll./2);
+    q = np.zeros(shape=(4))
+    q[0] = cos(pitch / 2) * cos(roll / 2)
+    q[1] = cos(pitch / 2) * sin(roll / 2)
+    q[2] = sin(pitch / 2) * cos(roll / 2)
+    q[3] = sin(pitch / 2) * sin(roll / 2)
+
+    return q
+
 
 def singleQuatToEuler(q: np.ndarray):
     if q.ndim != 1 or np.shape(q)[0] != 4:
@@ -25,5 +46,45 @@ def singleQuatToEuler(q: np.ndarray):
 def quatNormalise(q: np.ndarray):
     if q.ndim != 1 or np.shape(q)[0] != 4:
         raise ValueError('Invalid quaternion matrix')
+    
     norm = sqrt(q[0]**2 + q[1]**2 + q[2]**2 + q[3]**2)
     return q / norm
+
+def quatConj(q: np.ndarray):
+    if q.ndim != 1 or np.shape(q)[0] != 4:
+        raise ValueError('Invalid quaternion matrix')
+    
+    quat = np.ndarray(shape=(4))
+    quat[0] = q[0]
+    quat[1:] = -q[1:]
+    return quat
+
+def quatToDCM(q: np.ndarray):
+    if q.ndim != 1 or np.shape(q)[0] != 4:
+        raise ValueError('Invalid quaternion matrix')
+    
+    dcm = np.zeros(shape=(3,3))
+
+        
+    # R(1,1)= q(1)^2+q(2)^2-q(3)^2-q(4)^2;
+    # R(1,2)=-2*q(1)*q(4)+2*q(2)*q(3);
+    # R(1,3)= 2*q(1)*q(3)+2*q(2)*q(4);
+    dcm[0, 0] = q[0] ** 2 + q[1] ** 2 - q[2] ** 2 - q[3] ** 2
+    dcm[0, 1] = -2 * (q[0] * q[3] - q[1] * q[2])
+    dcm[0, 2] =  2 * (q[0] * q[2] + q[1] * q[3])
+
+    # R(2,1)= 2*q(1)*q(4)+2*q(2)*q(3);
+    # R(2,2)= q(1)^2-q(2)^2+q(3)^2-q(4)^2;
+    # R(2,3)=-2*q(1)*q(2)+2*q(3)*q(4);
+    dcm[1, 0] =  2 * (q[0] * q[3] + q[1] * q[2])
+    dcm[1, 1] = q[0] ** 2 - q[1] ** 2 + q[2] ** 2 - q[3] ** 2
+    dcm[1, 2] = -2 * (q[0] * q[1] - q[2] * q[3])
+
+    # R(3,1)=-2*q(1)*q(3)+2*q(2)*q(4);
+    # R(3,2)=2*q(1)*q(2)+2*q(3)*q(4);
+    # R(3,3)=q(1)^2-q(2)^2-q(3)^2+q(4)^2;
+    dcm[2, 0] = -2 * (q[0] * q[2] - q[1] * q[3])
+    dcm[2, 1] =  2 * (q[0] * q[1] + q[2] * q[3])
+    dcm[2, 2] = q[0] ** 2 - q[1] ** 2 - q[2] ** 2 + q[3] ** 2
+    
+    return dcm
